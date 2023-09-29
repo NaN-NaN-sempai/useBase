d1748b3d489a101d13e1fd70d70f573b64f1e8f8
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2023-09-29 18:08:58 -0300

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-import useBase from "./index.js";
-
-
-
-let valueInBinary = useBase("MYBASE").encode(42); // "101010"
-
-let valueInOctal = useBase.octal.encode(valueInBinary.raw); // 52
-
-console.log(valueInBinary);
